84ee9f30d4abf80c1c46e669cb3845de8ae7b854
QChris <Chris@QChrisdeiMac.local>
2016-09-06 21:59:25 +0800

    add

---
+++ 160906_FriendList/QcFriendList.h
@@ -0,0 +1,13 @@
+//
+//  QcFriendList.h
+//  160906_FriendList
+//
+//  Created by QChris on 16/9/6.
+//  Copyright © 2016年 Chrisqc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QcFriendList : NSObject
+
+@end
---
+++ 160906_FriendList/QcFriendList.m
@@ -0,0 +1,13 @@
+//
+//  QcFriendList.m
+//  160906_FriendList
+//
+//  Created by QChris on 16/9/6.
+//  Copyright © 2016年 Chrisqc. All rights reserved.
+//
+
+#import "QcFriendList.h"
+
+@implementation QcFriendList
+
+@end
